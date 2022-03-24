19aa1cd60e92b8f53dc9072bb1ecfe5b7310f261
Beimy <arbm159357@gmail.com>
2022-03-24 17:32:07 -0400

    laying out psuedocode, developing structure of timeblocks w/ bootstrap

---
+++ Develop/javascript.js
@@ -0,0 +1,11 @@
+// Load in timetable from local or create a new one
+
+// Display current day
+
+// Color rows based on time of day
+
+// Make descriptions clickable, turning them into a form
+
+// Return form to a <p> passing in new text
+
+// enable saving to local storage
