4d83e0f6af517d38455507e3dbfedc9bdbcd3c97
Beimy <arbm159357@gmail.com>
2022-03-24 19:08:21 -0400

    overall layout achieved, dynamically creating the schedule

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,5 +1,30 @@
 // Load in timetable from local or create a new one
 
+function generateSchedule() {
+    for(i = 0; i < 9; i++){
+        // create row div
+        $rowDiv = $("<div>").addClass("time-block row");
+
+        //create and append time sect to row div
+        $timeSect = $('<div>').addClass('col-sm-1 hour');
+        $timeSect.append('<p>TEXT</p>');
+        $rowDiv.append($timeSect);
+
+        //create and append description sect to row div
+        $descSect = $('<p>Hello</p>').addClass('col-sm-10 description');
+        $rowDiv.append($descSect);
+
+        //create and append button to row div
+        $button = $('<button type="button" name="save-btn" class="col-sm-1 saveBtn">');
+        $rowDiv.append($button);
+
+        //append rowDiv after each section is added to it
+        $('.container').append($rowDiv);
+        console.log($rowDiv);
+    }
+}
+
+generateSchedule();
 // Display current day
 
 // Color rows based on time of day
